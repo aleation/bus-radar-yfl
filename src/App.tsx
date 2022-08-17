87950c575dddd5e250fbc7b6605437952cb1ee2c
Yu Fong Lozano <yu.lozano.shen@gmail.com>
2022-08-17 21:08:59 +0200

    + Initial environment setup:React-TS/Tailwind + Added icons and fonts.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './assets/css/App.css';
+
+function App() {
+    return (
+        <div className="h-full bg-gradient-to-b from-slate-300 via-slate-100 to-slate-300 text-slate-800 flex flex-col items-center justify-center">
+            <div className="max-w-lg text-center space-y-4">
+                <h1 className="font-montserrat text-4xl font-bold mb-5">
+                    Tampere Bus Radar
+                </h1>
+                <p>Lorem ipsum dolor sit amet. Cum abactus cadunt, omnes caesiumes imitari superbus, flavum coordinataees. Sunt sectames transferre gratis, peritus brodiumes.</p>
+
+                <p>Cobaltum de secundus verpa, imperium acipenser! Cur compater ire? Fluctui, habena, et vox.</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
